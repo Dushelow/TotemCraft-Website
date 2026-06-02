de79d5564ecde819ae18b7fdbae1d31d56f1c8ac
Dushelow <secretstrawberry88@outlook.com>
2026-06-02 16:05:55 +0400

    uload index.php robots.txt wiki.php

---
+++ public_html/index.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+<title>TotemCraft - Ванильный сервер Minecraft</title>
+<meta charset="utf-8">
+<meta name="description" content="TotemCraft - ванильный сервер Minecraft 1.21.11 без вайпа, доната, приватов и лишних плагинов.">
+<meta name="keywords" content="minecraft, server, ванильный, сервер, майнкрафт, без привата, без доната, без вайпов, без сезонов, открытый, бесплатный">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="TotemCraft - Ванильный сервер Minecraft" />
+<meta property="og:description" content="TotemCraft - ванильный сервер Minecraft 1.21.11 без вайпа, доната, приватов и лишних плагинов." />
+<meta property="og:image" content="https://totemcraft.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://totemcraft.net" />
+<meta property="og:locale" content="ru_RU"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="TotemCraft" />
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<img src="/img/graphics/landing/hero-main.png" width="1" height="1" alt="preload"/>
+<img src="/img/videos/1.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/2.jpg" width="1" height="1" alt="Preload"/>
+<img src="/img/videos/3.jpg" width="1" height="1" alt="Preload"/>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div class="main-img-backdrop">
+		</div>
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						<div class='landing-ico-logo'>
+						</div>
+						<div class='landing-tx1-heading pulsate'>
+							<h1>TotemCraft<br>
+							 Ванильный сервер</h1>
+						</div>
+						<div class='landing-tx2-heading'>
+							<p>
+								 TotemCraft - ванильный сервер Minecraft 1.21.11 без вайпа, доната, приватов и лишних плагинов. В нашей группе Телеграм вы найдете чат, галерею и можете пронаблюдать за трансляцией игрового чата в реальном времени.
+							</p><br>
+							<p>
+								 Чтобы начать игру на сервере просто скопируйте IP адрес и подключайтесь.
+							</p>
+							<div class='landing-btn-container'>
+							  <div class="build-btn-button copy-ip-button">
+							    <div class="build-ico-button">
+							      <div class="build-ico-os">
+							        <img alt="Windows" src="img/icons/os/windows.png" style='height: 100%; width: 100%; object-fit: contain'/>
+							      </div>
+							    </div>
+							    <div class="build-tx1-button">
+							      <span>Скопировать IP: play.totemcraft.net</span>
+							    </div>
+							  </div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='landing-con-right content-remove '>
+					<div class='landing-img-hero' style="background: url(/img/graphics/landing/hero-main.png) no-repeat center; background-size: contain;">
+					</div>
+				</div>
+			    <div class="copy-notification">
+				  <span>IP скопирован в буфер обмена!</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="custom-banner-con-container darkmode-header">
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class="custom-header-block">
+			    <div class='banner-con-title fade-up-onstart'>
+			        <div class='banner-tx1-title fade-up-onstart'>
+			            <h1>Как устроена жизнь на сервере</h1>
+			        </div>
+			        <div class='banner-con-divider'>
+			        </div>
+			        <div class='banner-tx2-title fade-up-onstart'>
+			            <p>
+			                Ознакомьтесь, чтобы погрузиться в наш мир
+			            </p>
+			        </div>
+			    </div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Идея и геймплей</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									TotemCraft - это классический сервер старой школы. Основная идея - застройка карты красивыми проектами игроков и история которую игроки создают сами. Мы не делаем вайп основного мира. У нас отсутствуют привилегии и креатив, все делается в выживании.<br>
+									<br>
+									На сервере нет телепортов, поэтому мы рекомендуем пользоваться ванильной механикой порталов - через построенное игроками адское метро (незер хаб). <br>
+									<br>
+									Спавн используется как торговая зона - в радиусе 500 игроки могут построить магазины, предварительно получив разрешение от администрации. Несогласованные постройки сносятся после предупреждения.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/project.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/history.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Кланы и сражения</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>В игре нет PvP и фракционных режимов, но для легкого политического RolePlay была организована система кланов. Цель клана - объединение игроков с общей идеей и историей. Основное их преимущество - возможность проводить PvP сражения.</p>
+								<br>
+								<p>Правила проведения войн: перед сражением лидеры кланов обязаны письменно зафиксировать условия конфликта с помощью книги и пера. Проигравшие выполняют условия победителей. Каждая сторона подписывает свой экземпляр и обменивается им с оппонентом. Убийства игроков вне войны запрещено и карается баном.</p>
+								<br>
+								<p>Для создания клана обратитесь к администрации: у вас должно быть минимум трое участников, лидером наиграно от 120+ часов на сервере (команда /playtime <ник>), клановая база и свой флаг.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Правила и организация</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>• Запрещено воровать и портить чужое имущество</p>
+								<p>• Запрещены читы</p>
+								<p>• Запрещено PvP без обоюдного согласия</p>
+								<p>• Запрещена реклама</p>
+								<p>• Запрещено агрессивное обсуждение политики</p>
+								<p>• Запрещено оскорбление родных</p><br>
+						        <p>
+						            Жалобы, предложения и вопросы игроков принимаются в группе Телеграм и в Дискорде.
+						        </p><br>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/team.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/plugins.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Плагины и команды</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>Мы используем минимальный набор плагинов, чтобы сохранить ванильность, но обеспечить безопасность и создать небольшой элемент ролевой игры. Плагины:</p>
+								<p>• CoreProtect</p>
+								<p>• PlasmoVoice</p>
+								<p>• SimpleClans</p>
+								<p>• Shopkeepers</p>
+								<p>• LWC</p><br>
+								<p>Более подробно с командами сервера вы можете ознакомиться в закрепленных сообщениях в нашей группе Телеграм или в Дискорде.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='container-con-block darkmode-block'>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ public_html/wiki.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="ru-RU">
+<head>
+<title>TotemCraft - Wiki</title>
+<meta charset="utf-8">
+<meta name="description" content="TotemCraft - ванильный сервер Minecraft 1.21.11 без вайпа, доната, приватов и лишних плагинов.">
+<meta name="keywords" content="minecraft, server, ванильный, сервер, майнкрафт, без привата, без доната, без вайпов, без сезонов, открытый, бесплатный">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="TotemCraft - Ванильный сервер Minecraft" />
+<meta property="og:description" content="TotemCraft - ванильный сервер Minecraft 1.21.11 без вайпа, доната, приватов и лишних плагинов." />
+<meta property="og:image" content="https://totemcraft.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://totemcraft.net" />
+<meta property="og:locale" content="ru_RU"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="TotemCraft" />
+
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div class="about-img-backdrop">
+		</div>
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Страница в разработке</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 ...
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+		</div>
+	</div>
+</div>
+</body>
+</html>
