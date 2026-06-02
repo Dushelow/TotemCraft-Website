0b5501a1b1a6094e8ec191682258915589dd2eaf
Dushelow <secretstrawberry88@outlook.com>
2026-06-02 16:06:43 +0400

    upload lib folder and content

---
+++ public_html/lib/module/inc-menu.php
@@ -0,0 +1,85 @@
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<a href="/">
+			<div class="menu-btn-select">
+				<span>Главная</span>
+			</div>
+			<div class="menu-con-divider">
+			</div>
+			</a>
+ 			<a href='https://wiki.totemcraft.net' target="_blank">
+			<div class="menu-btn-select">
+				<span>Wiki</span>
+			</div>
+			</a>
+			<a href='https://discord.gg/MWeUjNWJG3' target="_blank">
+			<div class="menu-btn-select">
+				<span>Discord</span>
+			</div>
+			</a>
+			<a href='https://t.me/totemcraftnet' target="_blank">
+			<div class="menu-btn-select">
+				<span>Чат в Telegram</span>
+			</div>
+			</a>
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Меню</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Главная</span>
+				</div>
+				</a>
+ 				<a href="https://wiki.totemcraft.net" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Wiki</span>
+				</div>
+				</a>
+				<a href="https://discord.gg/MWeUjNWJG3" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Discord</span>
+				</div>
+				</a>
+				<a href="https://t.me/totemcraftnet" target="_blank">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Чат в Telegram</span>
+				</div>
+				</a>
+				<div class='mobile-menu-btn-spacer'>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
